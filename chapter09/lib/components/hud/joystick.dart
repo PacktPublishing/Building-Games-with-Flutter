548b1f6fcd311493ebe3e9811d728f3dd34cccba
tnsoftbear <i.kotlevskis@bidpath.com>
2023-05-30 10:36:48 +0300

    Delete examples of chapters 1 - 10

--- chapter09/lib/components/hud/joystick.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flame/components.dart';
-
-class Joystick extends JoystickComponent {
-
-  Joystick({required PositionComponent knob, PositionComponent? background, Vector2? position}) : super (knob: knob, background: background, position: position);
-}
